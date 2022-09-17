f22a0b06f2ee4bfbdcbab1c2d68bafe7cd537304
DonPastor <marijan.pastor@gmail>
2022-09-17 20:25:30 +0200

    Start

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomePage from "../pages/HomePage.vue";
+import AboutPage from "../pages/HomePage.vue";
+import BrazilPage from "../pages/BrazilPage";
+import HawaiPage from "../pages/HawaiPage";
+import JamaicaPage from "../pages/JamaicaPage";
+import PanamaPage from "../pages/PanamaPage";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+    {
+        path: "/",
+        name: "home",
+        component: HomePage,
+    },
+    {
+        path: "/about",
+        name: "about",
+        component: AboutPage,
+    },
+    {
+        path: "/brazil",
+        name: "brazil",
+        component: BrazilPage,
+    },
+    {
+        path: "/hawai",
+        name: "hawai",
+        component: HawaiPage,
+    },
+    {
+        path: "/jamaica",
+        name: "jamaica",
+        component: JamaicaPage,
+    },
+    {
+        path: "/panama",
+        name: "panama",
+        component: PanamaPage,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
