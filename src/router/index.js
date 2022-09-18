7f9759900a2a3db7bfc2f0a81c355336da392f5a
DonPastor <marijan.pastor@gmail>
2022-09-18 19:38:51 +0200

    Cleaning code

--- src/router/index.js
+++ src/router/index.js
@@ -2,29 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 
 const routes = [
-    { path: "/", name: "home", component: HomePage },
-    {   
-        path: "/brazil", 
-        name: "brazil",
-        component: () => import ("../pages/BrazilPage.vue")
-    },
     { 
-        path: "/hawaii", 
-        name: "hawaii", 
-        component: () => import ("../pages/HawaiiPage") 
-    },
-    { 
-        path: "/jamaica", 
-        name: "jamaica", 
-        component: () => import ("../pages/JamaicaPage") 
-    },
-    { 
-        path: "/panama", 
-        name: "panama", 
-        component: () => import ("../pages/PanamaPage") 
+        path: "/", 
+        name: "home", 
+        component: HomePage 
     },
     {
-        path: "/destination/:id", name:"destination.show", component: ()=>import('@/pages/DestinationShow.vue')
+        path: "/destination/:id/:slug", 
+        name:"destination.show", 
+        component: ()=>import('@/pages/DestinationShow.vue')
     }
 ];
 
