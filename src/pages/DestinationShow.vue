573bb7d25cb437eee1b5dabeac01aa450d1ced0e
DonPastor <marijan.pastor@gmail>
2022-09-18 23:01:43 +0200

    Reacting to Param Changes 3

--- src/pages/DestinationShow.vue
+++ src/pages/DestinationShow.vue
@@ -9,28 +9,17 @@
 </template>
 
 <script>
-    // import sourceData from "@/data.json"
+    import sourceData from "@/data.json"
 
     export default {
-        data (){
-            return {
-                destination: null
-            }
-        },
+
         computed:{
             destinationId(){
                 return parseInt(this.$route.params.id) 
             },
-        },
-        methods:{
-            async initData(){
-                const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}`)
-                this.destination = await response.json()
+            destination(){
+                return sourceData.destinations.find((destination) => destination.id == this.destinationId)
             }
-        },
-        async created(){
-            this.initData()
-            this.$watch(() => this.$route.params, this.initData)
         }
     }
 </script>
